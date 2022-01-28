b2e1ed3a41091cfe2c355d15e4fd5e20fed6eb0b
Alexander Baron <alexander.baron1@rwth-aachen.de>
2022-01-28 17:48:28 +0100

    Infos about activities outside university

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,26 @@
 export default function Index() {
   return (
-    <main className="space-y-8">
+    <main>
       <h1>Alexander Baron</h1>
       <p>
         CS Student at <a href="https://rwth-aachen.de/">RWTH Aachen</a> and Student Assistant at the{' '}
         <a href="https://embedded.rwth-aachen.de/">Chair for Embedded Systems</a>.
       </p>
+      <h2>Apart from being a Student, I am also...</h2>
+      <ul>
+        <li>Member of the Network Group of my Student Dorm</li>
+        <li>
+          Currently in Basic Training at the{' '}
+          <a href="https://www.thw.de/EN/Homepage/homepage_node.html">THW (German catastrophe prevention unit)</a>
+        </li>
+        <li>
+          Active in the{' '}
+          <a href="https://www.studienstiftung.de/stipendiaten/botschafterinnen-und-botschafter/">
+            "Ambassador Programme"
+          </a>{' '}
+          of the <a href="https://www.studienstiftung.de/en/">Studienstiftung des deutschen Volkes</a>
+        </li>
+      </ul>
     </main>
   );
 }
